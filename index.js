d7ee241ee76fc2f4c3386ea116d5361b88b0bdfd
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2022-10-22 13:51:09 +0400

    calculator in progress

---
+++ index.js
@@ -0,0 +1,28 @@
+function calcAdd() {
+    let a = +prompt(`First number`); 
+    let b = +prompt(`Second number`);
+    
+    alert(`Result ${a + b}`);
+}
+
+function calcSub() {
+    let a = +prompt(`First number`); 
+    let b = +prompt(`Second number`);
+
+    alert(`Result ${a - b}`);
+}
+
+function calcMult() {
+    let a = +prompt(`First number`); 
+    let b = +prompt(`Second number`);
+
+    alert(`Result ${a * b}`);
+}
+
+function calcDiv() {
+    let a = +prompt(`First number`); 
+    let b = +prompt(`Second number`);
+
+    alert(`Result ${a / b}`);
+}
+
