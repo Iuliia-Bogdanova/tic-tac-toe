fac2bc23abd62424414047abf51fe356cb2f108c
Iuliia-Bogdanova <u-bogdanova@yandex.ru>
2022-10-23 21:29:48 +0400

    did partially

--- index.js
+++ index.js
@@ -22,7 +22,11 @@ function calcMult() {
 function calcDiv() {
     let a = +prompt(`First number`); 
     let b = +prompt(`Second number`);
+    
+        if (b == 0) {
+            
+            alert('You can not divide by 0');
+        }
 
     alert(`Result ${a / b}`);
 }
-
